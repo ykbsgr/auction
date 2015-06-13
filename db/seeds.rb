c43e02d35d252a4fec416b95caefb3e3339d890e
矢加部 卓 <ykbsgr@gmail.com>
2015-06-13 15:12:37 +0900

    seedsファイル作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Item.create(
+  name: "エアマックス 95",
+  description: "1995年のランニングマックスモデルの復刻版。時代を席巻した名モデルAIR MAX 95。当時は斬新でハイテクなルックスがストリートのファッションシーンを席巻しました。",
+  price: 25000,
+  seller_id: 746,
+  email: "daisuke@nashiro.org",
+  image_url: "http://news.digitallab.biz/wordpress/wp-content/uploads/Nike-Air-Max-95-Neon.jpg"
+)
+
+Item.create(
+  name: "フットスケープ",
+  description: "横にシューレースがある斬新なモデル。大切にしていただける方にお譲りします。",
+  price: 18000,
+  seller_id: 32,
+  email: "foo@example.com",
+  image_url: "http://www.furugikaitori.co.jp/auction/item/F351-1.jpg"
+)
+
+item3 = Item.new(
+  name: "ポンプフューリー",
+  description: "ポンプを押すと、空気によってパイプが膨らみ足型にフィットします。",
+  price: 18000,
+  seller_id: 32,
+  email: "foo@example.com",
+  image_url: "http://image.rakuten.co.jp/atmos-girls/cabinet/reebok/v47514-1.jpg"
+)
+
+item3.save
+
+
