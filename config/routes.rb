595bf1483d712f1c80502d1f1a6a3f3600473e8b
矢加部 卓 <ykbsgr@gmail.com>
2015-06-13 16:48:32 +0900

    一つ戻る

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get 'items/:id' => 'items#show'
   get 'items'=> 'items#index'
 
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
